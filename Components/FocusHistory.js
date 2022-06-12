e9b2bb736819f64fcf384f5076dac473a8a2ddc3
Aditya Bal Gupta <guptaaditya70993@gmail.com>
2022-06-12 17:53:27 +0530

    ..

--- Components/FocusHistory.js
+++ Components/FocusHistory.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { StyleSheet, Text, View, FlatList } from 'react-native';
-import { ScrollView } from 'react-native-web';
 import Button from "./Button";
 
 export default function FocusHistory(props) {
